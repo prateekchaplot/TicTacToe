70b4d7190d429ebd6ea1cee607c33585519580ea
Prateek Chaplot <prateekchaplot96@gmail.com>
2023-01-05 10:31:01 +0530

    Add changes to play tic tac toe

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Create a game and two players
+using TicTacToe.Models;
+
+var board = new char[3, 3];
+Game game = new Game(new List<Player>(), board);
+Player player1 = new Player("Player 1", 'X', board);
+Player player2 = new Player("Player 2", 'O', board);
+
+// Add the players to the game
+game.AddPlayer(player1);
+game.AddPlayer(player2);
+
+// Start the game
+while (!game.IsGameOver())
+{
+    // Notify the players
+    game.NotifyPlayers();
+}
+
+// Display the result of the game
+Console.WriteLine("Game Over!");
+if (game.CheckForWinner())
+{
+    Console.WriteLine("We have a winner!");
+}
+else
+{
+    Console.WriteLine("It's a draw!");
+}
